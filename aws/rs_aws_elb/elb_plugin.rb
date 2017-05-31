3553946a7c767185b651d50ab549a65520f3e356
Curt Wilhelm <cdwilhelm@users.noreply.github.com>
2017-05-31 12:46:26 -0600

    add package declaration

--- aws/rs_aws_elb/elb_plugin.rb
+++ aws/rs_aws_elb/elb_plugin.rb
@@ -2,7 +2,7 @@ name 'aws_elb_plugin'
 type 'plugin'
 rs_ca_ver 20161221
 short_description "Amazon Web Services - Elastic Load Balancer"
-
+package "plugin/rs_aws_elb"
 import "sys_log"
 
 plugin "rs_aws_elb" do
