375ff475397a5ff66ee3ac98c3978a7e03db7bca
srpomeroy <sean.pomeroy@rightscale.com>
2017-08-04 13:25:10 -0400

    create database server

--- azure/rs_azure_mysql/azure_mysql_plugin.rb
+++ azure/rs_azure_mysql/azure_mysql_plugin.rb
@@ -32,7 +32,7 @@ plugin "rs_azure_mysql" do
 
   type "mysql_server" do
     href_templates "{{type=='Microsoft.DBforMySQL/servers' && id || null}}"
-    provision "provision_resource"
+    provision "provision_server"
     delete    "delete_resource"
 
     field "properties" do
@@ -66,30 +66,42 @@ plugin "rs_azure_mysql" do
     end
 
     action "create" do
-      type "sql_server"
+      type "mysql_server"
       path "/subscriptions/$subscription_id/resourceGroups/$resource_group/providers/Microsoft.DBforMySQL/servers/$name"
       verb "PUT"
     end
 
     action "show" do
-      type "sql_server"
-      path "$href"
+      type "mysql_server"
+      path "/subscriptions/$subscription_id/resourceGroups/$resource_group/providers/Microsoft.DBforMySQL/servers/$name"
       verb "GET"
+      
+      field "resource_group" do
+        location "path"
+      end 
+
+      field "name" do
+        location "path"
+      end
     end
 
     action "get" do
-      type "sql_server"
+      type "mysql_server"
       path "$href"
       verb "GET"
     end
 
     action "destroy" do
-      type "sql_server"
+      type "mysql_server"
       path "$href"
       verb "DELETE"
     end
 
-    output "id","name","type","location","kind"
+    output "id","name","type","location","tags","sku","properties"
+
+    output "state" do
+      body_path "properties.userVisibleState"
+    end
 
     output "fullyQualifiedDomainName" do
       body_path "properties.fullyQualifiedDomainName"
@@ -103,45 +115,30 @@ plugin "rs_azure_mysql" do
       body_path "properties.administratorLoginPassword"
     end
 
-    output "externalAdministratorLogin" do
-      body_path "properties.externalAdministratorLogin"
+    output "sslEnforcement" do
+      body_path "properties.sslEnforcement"
     end
 
-    output "externalAdministratorSid" do
-      body_path "properties.externalAdministratorSid"
+    output "userVisibleState" do
+      body_path "properties.userVisibleState"
     end
 
     output "version" do
       body_path "properties.version"
     end
 
-    output "state" do
-      body_path "properties.state"
-    end
-
     link "databases" do
-      path "$id/databases?api-version=2014-04-01"
+      path "$id/databases"
       type "databases"
       output_path "value[*]"
     end
 
     link "firewall_rules" do
-      path "$id/firewallRules?api-version=2014-04-01"
+      path "$id/firewallRules"
       type "firewall_rule"
       output_path "value[*]"
     end
 
-    link "failover_groups" do
-      path "$id/failoverGroups?api-version=2015-05-01-preview"
-      type "failover_group"
-      output_path "value[*]"
-    end
-
-    link "elastic_pools" do
-      path "$id/elasticPools?api-version=2014-04-01"
-      type "elastic_pool"
-      output_path "value[*]"
-    end
   end
 
   type "databases" do
@@ -154,10 +151,10 @@ plugin "rs_azure_mysql" do
       location "body"
     end
 
-    field "location" do
-      type "string"
-      location "body"
-    end
+    #field "location" do
+    #  type "string"
+    #  location "body"
+    #end
 
     field "resource_group" do
       type "string"
@@ -176,7 +173,7 @@ plugin "rs_azure_mysql" do
 
     action "create" do
       type "databases"
-      path "/subscriptions/$subscription_id/resourceGroups/$resource_group/providers/Microsoft.DBforMySQL/servers/$server_name/databases/$name?api-version=2014-04-01"
+      path "/subscriptions/$subscription_id/resourceGroups/$resource_group/providers/Microsoft.DBforMySQL/servers/$server_name/databases/$name"
       verb "PUT"
     end
 
@@ -192,27 +189,27 @@ plugin "rs_azure_mysql" do
       verb "DELETE"
     end
     
-    action "pause" do
-      type "databases"
-      path "$href/pause"
-      verb "POST"
-    end
+    #action "pause" do
+    #  type "databases"
+    #  path "$href/pause"
+    #  verb "POST"
+    #end
 
-    action "resume" do
-      type "databases"
-      path "$href/resume"
-      verb "POST"
-    end
+    #action "resume" do
+    #  type "databases"
+    #  path "$href/resume"
+    #  verb "POST"
+    #end
 
     action "update" do
       type "databases"
       path "$href"
-      verb "PATCH"
+      verb "PUT"
     end
 
     action "show" do
       type "databases"
-      path "/subscriptions/$subscription_id/resourceGroups/$resource_group/providers/Microsoft.DBforMySQL/servers/$server_name/databases/$name?api-version=2014-04-01"
+      path "/subscriptions/$subscription_id/resourceGroups/$resource_group/providers/Microsoft.DBforMySQL/servers/$server_name/databases/$name"
       verb "GET"
 
       field "resource_group" do
@@ -228,77 +225,21 @@ plugin "rs_azure_mysql" do
       end
     end
 
-    output "id","name","type","location","kind"
-
-    output "edition" do
-      body_path "properties.edition"
-    end
-
-    output "status" do
-      body_path "properties.status"
-    end
-
-    output "serviceLevelObjective" do
-      body_path "properties.serviceLevelObjective"
-    end
+    output "id","name","type"
 
     output "collation" do
       body_path "properties.collation"
     end
 
-    output "creationDate" do
-      body_path "properties.creationDate"
-    end
-
-    output "maxSizeBytes" do
-      body_path "properties.maxSizeBytes"
-    end
-
-    output "currentServiceObjectiveId" do
-      body_path "properties.currentServiceObjectiveId"
-    end
-
-    output "requestedServiceObjectiveId" do
-      body_path "properties.requestedServiceObjectiveId"
-    end
-
-    output "requestedServiceObjectiveName" do
-      body_path "properties.requestedServiceObjectiveName"
-    end
-
-    output "sampleName" do
-      body_path "properties.sampleName"
-    end
-
-    output "defaultSecondaryLocation" do
-      body_path "properties.defaultSecondaryLocation"
-    end
-
-    output "earliestRestoreDate" do
-      body_path "properties.earliestRestoreDate"
-    end
-
-    output "elasticPoolName" do
-      body_path "properties.elasticPoolName"
-    end
-
-    output "containmentState" do
-      body_path "properties.containmentState"
-    end
-
-    output "readScale" do
-      body_path "properties.readScale"
-    end
-
-    output "failoverGroupId" do
-      body_path "properties.failoverGroupId"
+    output "charset" do
+      body_path "properties.charset"
     end
 
   end
 
   type "firewall_rule" do
     href_templates "{{type=='Microsoft.DBforMySQL/servers/firewallRules' && id || null}}","{{value[0].type=='Microsoft.DBforMySQL/servers/firewallRules' && id || null}}"
-    provision "provision_firewall_rule"
+    provision "provision_resource"
     delete    "delete_resource"
 
     field "properties" do
@@ -306,10 +247,10 @@ plugin "rs_azure_mysql" do
       location "body"
     end
 
-    field "location" do
-      type "string"
-      location "body"
-    end
+    #field "location" do
+    #  type "string"
+    #  location "body"
+    #end
 
     field "resource_group" do
       type "string"
@@ -327,16 +268,19 @@ plugin "rs_azure_mysql" do
     end
 
     action "create" do
+      type "firewall_rule"
       path "/subscriptions/$subscription_id/resourceGroups/$resource_group/providers/Microsoft.DBforMySQL/servers/$server_name/firewallRules/$name"
       verb "PUT"
     end
 
     action "get" do
+      type "firewall_rule"
       path "$href"
       verb "GET"
     end
 
     action "show" do
+      type "firewall_rule"
       path "$href"
       verb "GET"
     end
@@ -347,12 +291,13 @@ plugin "rs_azure_mysql" do
     end
     
     action "list" do
+      type "firewall_rule"
       path "/subscriptions/$subscription_id/resourceGroups/$resource_group/providers/Microsoft.DBforMySQL/servers/$server_name/firewallRules"
       verb "GET"
-      output_path "properties.percentComplete"
+      output_path "value[*]"
     end
 
-    output "id","name","type","location","kind"
+    output "id","name","type"
 
     output "startIpAddress" do
       body_path "properties.startIpAddress"
@@ -371,7 +316,7 @@ resource_pool "rs_azure_mysql" do
     end
 
     auth "azure_auth", type: "oauth2" do
-      token_url "https://login.microsoftonline.com/AZURE_TENANT_ID/oauth2/token"
+      token_url "https://login.microsoftonline.com/09b8fec1-4b8d-48dd-8afa-5c1a775ea0f2/oauth2/token"
       grant type: "client_credentials" do
         client_id cred("AZURE_APPLICATION_ID")
         client_secret cred("AZURE_APPLICATION_KEY")
@@ -418,6 +363,59 @@ define provision_resource(@declaration) return @resource do
   end
 end
 
+define provision_server(@declaration) return @resource do
+  sub on_error: stop_debugging() do
+    $object = to_object(@declaration)
+    $fields = $object["fields"]
+    $type = $object["type"]
+    $name = $fields["name"]
+    $resource_group = $fields["resource_group"]
+    call sys_log.set_task_target(@@deployment)
+    call sys_log.summary(join(["Provision ", $type]))
+    call sys_log.detail($object)
+    call start_debugging()
+    @operation = rs_azure_mysql.$type.create($fields)
+    call sys_log.detail(to_object(@operation))
+    call stop_debugging()
+    call sys_log.detail("entering check for database created")
+    $attempts = 0
+    sub on_error: handle_retries($attempts), timeout: 60m do
+      $attempts = $attempts + 1
+      call sys_log.detail("sleeping 10, db server not created")
+      sleep(10)
+      call start_debugging()
+      @new_resource = @operation.show(name: $name, resource_group: $resource_group )
+      call stop_debugging()
+    end
+    call start_debugging()
+    @new_resource = @operation.show(name: $name, resource_group: $resource_group)
+    $status = @new_resource.state
+    call sys_log.detail("Checking that database state is online")
+    sub on_error: skip, timeout: 60m do
+      while $status != "Ready" do
+        $status = @new_resource.state
+        call stop_debugging()
+        call sys_log.detail(join(["Status: ", $status]))
+        call start_debugging()
+        sleep(10)
+      end
+    end
+    @resource = @new_resource
+    call sys_log.detail(to_object(@resource))
+    call stop_debugging()
+  end
+end
+
+define handle_retries($attempts) do
+  if $attempts <= 36
+    call sys_log.detail("error:"+$_error["type"] + ": " + $_error["message"])
+    log_error($_error["type"] + ": " + $_error["message"])
+    $_error_behavior = "retry"
+  else
+    raise $_errors
+  end
+end
+
 define provision_database(@declaration) return @resource do
   sub on_error: stop_debugging() do
     $object = to_object(@declaration)
@@ -435,7 +433,9 @@ define provision_database(@declaration) return @resource do
     $server_name = $fields["server_name"]
     $resource_group = $fields["resource_group"]
     call sys_log.detail("entering check for database created")
-    sub on_error: retry, timeout: 60m do
+    $attempts = 0
+    sub on_error: handle_retries($attempts), timeout: 60m do
+      $attempts = $attempts + 1
       call sys_log.detail("sleeping 10")
       sleep(10)
       @new_resource = @operation.show(name: $name, server_name: $server_name, resource_group: $resource_group )
