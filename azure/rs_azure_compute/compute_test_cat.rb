2d351ff137644fa5003864e8cd54ce7645e583dc
Richard Shade <rshade@rightscale.com>
2017-08-16 15:28:14 -0500

    adding in vmsizes

--- azure/rs_azure_compute/compute_test_cat.rb
+++ azure/rs_azure_compute/compute_test_cat.rb
@@ -8,6 +8,21 @@ parameter "subscription_id" do
   like $rs_azure_compute.subscription_id
 end
 
+parameter "vmSize" do
+  label "VirtualMachine Sizes"
+  type "string"
+  allowed_values "Standard_F1","Standard_F2","Standard_F4","Standard_F8","Standard_F16"
+  default "Standard_F1"
+end
+
+parameter "vmSize2" do
+  label "VMS"
+  type "string"
+  description "json:{\"definition\":\"getSizes\", \"description\": \"Pick the vmSize\"}"
+  operations "change_size"
+end
+
+
 permission "read_creds" do
   actions   "rs_cm.show_sensitive","rs_cm.index_sensitive"
   resources "rs_cm.credentials"
@@ -15,6 +30,10 @@ end
 
 output "vm_ids" do
   label "VM ids"
+end
+
+output "vm_sizes" do
+  label "Available VM Sizes"
 end
 
 resource "my_availability_group", type: "rs_azure_compute.availability_set" do
@@ -50,7 +69,7 @@ resource "server1", type: "server" do
   multi_cloud_image_href "/api/multi_cloud_images/423486003"
   network "ARM-CentralUS"
   subnets "default"
-  instance_type "Standard_F1"
+  instance_type $vmSize
   security_groups "Default"
   associate_public_ip_address true
   cloud_specific_attributes do {
@@ -80,11 +99,17 @@ operation "launch" do
  description "Launch the application"
  definition "launch_handler"
  output_mappings do {
-    $vm_ids => $vms
+    $vm_ids => $vms,
+    $vm_sizes => $vmss
   } end
 end
 
-define launch_handler(@my_availability_group,@server1,@my_vm_extension,@deployment_availability_group) return @my_availability_group,@my_availability_group,@server1,@my_vm_extension,@deployment_availability_group,$vms do
+operation "change_size" do
+  description "changes size"
+  definition "supersize_me"
+end
+
+define launch_handler(@my_availability_group,@server1,@my_vm_extension,@deployment_availability_group) return @my_availability_group,@my_availability_group,@server1,@my_vm_extension,@deployment_availability_group,$vms,$vmss do
   call start_debugging()
   provision(@my_availability_group)
   provision(@server1)
@@ -98,6 +123,9 @@ define launch_handler(@my_availability_group,@server1,@my_vm_extension,@deployme
   $vms = $vms + to_s(@deployment_ag.virtualmachines)
   call sys_log.detail("vms:" + to_s($vms))
   @vm=rs_azure_compute.virtualmachine.show(resource_group: $resource_group, virtualMachineName: @server1.name)
+  $vmss = @vm.vmSizes()
+  call sys_log.detail("sizes:" + to_s($vmss))
+  $vmss=to_s($vmss)
   $vm_object=to_object(@vm)
   $vm_fields=$vm_object["details"][0]
   $vm_fields["properties"]["diagnosticsProfile"]={}
@@ -106,6 +134,33 @@ define launch_handler(@my_availability_group,@server1,@my_vm_extension,@deployme
   $vm_fields["properties"]["diagnosticsProfile"]["bootDiagnostics"]["storageUri"] = "https://dftestingdiag134.blob.core.windows.net"
   @vm.update($vm_fields)
   call stop_debugging()
+end
+
+define getSizes() return $values do
+  call start_debugging()
+  sub on_error: stop_debugging() do
+    @vm=rs_azure_compute.virtualmachine.show(resource_group: @@deployment.name, virtualMachineName: join(["server1-", last(split(@@deployment.href, "/"))]))
+    $$vmss = @vm.vmSizes()
+  end
+  call stop_debugging()
+  call sys_log.detail("sizes:" + to_s($$vmss))
+  $values=[]
+  $values << "Standard_F1"
+  foreach $size in $$vmss[0]["value"] do
+    $values << $size["name"]
+  end
+
+end
+
+define supersize_me(@server1,$vmSize) return @server1 do
+  @vm=rs_azure_compute.virtualmachine.show(resource_group: @@deployment.name, virtualMachineName: @server1.name)
+  $vm_object=to_object(@vm)
+  $vm_fields=$vm_object["details"][0]
+  $vm_fields["properties"]["hardwareProfile"]={}
+  $vm_fields["properties"]["hardwareProfile"]["vmSize"] = $vmSize
+  @vm.update($vm_fields)
+  sleep(60)
+  sleep_until(@server1.state == 'operational')
 end
 
 define start_debugging() do
