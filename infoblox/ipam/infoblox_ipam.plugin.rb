278574892ac2c4bbe5e2c8ecd93663f9a63bfe65
MitchellGerdisch <mitchell.gerdisch@rightscale.com>
2017-09-06 21:19:00 -0500

    added fields and new search action and updated READMEs

--- infoblox/ipam/infoblox_ipam.plugin.rb
+++ infoblox/ipam/infoblox_ipam.plugin.rb
@@ -28,21 +28,27 @@ plugin "rs_infoblox_ipam" do
     path "_token/$tunnel_token/wapi/v2.2.2/"
   end
   
+  # Infoblox record:host object
+  # https://ipam.illinois.edu/wapidoc/objects/record.host.html
   type "record_host" do
     href_templates "/{{_ref}}"  # The leading slash is makes the reference that comes back from infoblox href-like
     provision "provision_record_host"
     delete "delete_record_host"
  
     # The FQDN of the host to which the IP is being allocated.
-    # It must contain the zone (domain) and the infoblox service account must have write permissions.
+    # It must contain the zone (domain) being used.
+    # The infoblox service account must have write permissions to the given zone.
     # E.g. myserver.example.com where myserver is the host name and example.com is the zone.
     field "name" do
       type "string"
       location "body"
       required true
     end
-    
+       
     # An array of hashes.
+    # See the record:host_ipv4addr object definition for fields you can pass in the hashes.
+    #   REF: https://ipam.illinois.edu/wapidoc/objects/record.host_ipv4addr.html
+    # 
     # To get the next available IP use this (where the CIDR is a network to which the infoblox service account has write access).
     #   ipv4addrs [{ "ipv4addr":"func:nextavailableip:10.1.124.0/24" }]
     # To get a specific IP use this (again the IP address must be in network to which the service account has access.
@@ -50,18 +56,105 @@ plugin "rs_infoblox_ipam" do
     field "ipv4addrs" do
       type "array"
       location "body"
-      required true
+      required false  # An ipv4addrs object or ipv6addrs object is required
+    end
+    
+    # Like ipv4addrs - but these go up to a quinary 11
+    field "ipv6addrs" do
+      type "array"
+      location "body"
+      required false
+    end
+    
+    # The following fields are not required and represent most of the API-supported parameters.
+    # Refer to https://ipam.illinois.edu/wapidoc/objects/record.host.html#fields
+    field "aliases" do
+      type "array"
+      location "body"
+      required false
+    end
+    
+    field "allow_telnet" do
+      type "boolean"
+      location "body"
+      required false
+    end
+    
+    field "comment" do
+      type "string"
+      location "body"
+      required false
+    end
+    
+    field "configure_for_dns" do
+      type "boolean"
+      location "body"
+      required false
+    end
+    
+    field "device_description" do
+      type "string"
+      location "body"
+      required false
+    end
+    
+    field "device_location" do
+      type "string"
+      location "body"
+      required false
+    end
+    
+    field "device_type" do
+      type "string"
+      location "body"
+      required false
+    end
+    
+    field "device_vendor" do
+      type "string"
+      location "body"
+      required false
+    end
+    
+    field "disable" do
+      type "boolean"
+      location "body"
+      required false
+    end
+    
+    field "disable_discovery" do
+      type "boolean"
+      location "body"
+      required false
+    end
+    
+    field "dns_aliases" do
+      type "array"
+      location "body"
+      required false
+    end
+    
+    field "use_ttl" do
+      type "boolean"
+      location "body"
+      required false
+    end
+    
+    field "ttl" do
+      type "number"
+      location "body"
+      required false
     end
     
     action "show", "destroy" 
       
     action "create" do
       verb "POST"
-      path "/record:host?_return_fields=ipv4addrs" 
+      path "/record:host?_return_fields=ipv4addrs" # This query string is essential for json response
     end
     
-    # Returns an array of a single array of host record hashes
-    # TODO: Have the action return just the array of hashes. 
+    # Returns an ARRAY of a single ARRAY of host record HASHES
+    # TODO: Figure out how to have it just return the array of hashes. 
     action "list_by_name" do
       verb "GET"
       path "/record:host?name~=$name_filter"
@@ -72,17 +165,40 @@ plugin "rs_infoblox_ipam" do
       end
     end
     
-    output "host_ref" do
+    # Generic search action. The list_by_name action above is a specific example of this.
+    # Returns an ARRAY of a single ARRAY of host record HASHES
+    # Examples:
+    #   ipv4addr~=10\..*\.124\..* Finds any host record with an ip address of the form 10.X.124.X
+    #   name:=MyServer.example.com  Case insenstive search (:=) for any host record with a name that matches myserver.example.com 
+    # See the following document for which fields are searchable and how.
+    #   https://ipam.illinois.edu/wapidoc/objects/record.host.html#fields-list 
+    # TODO: Figure out how to have it just return the array of hashes. 
+    action "search" do
+      verb "GET"
+      path "/record:host?$search_string"
+      type "array"
+      
+      field "search_string" do   
+        location "path"
+      end
+    end
+    
+    output "_ref" do
       body_path "_ref"
     end
+    
+    output "name" do
+      body_path "name"
+    end
 
-    output "host_addr" do
+    output "ipv4addr" do
       body_path "ipv4addrs[0].ipv4addr"
     end
     
-    output "host_name" do
-      body_path "ipv4addrs[0].host"
+    output "ipv6addr" do
+      body_path "ipv6addrs[0].ipv6addr"
     end
+
   end    
 end
 
--- infoblox/ipam/test_infoblox_ipam.cat.rb
+++ infoblox/ipam/test_infoblox_ipam.cat.rb
@@ -13,7 +13,12 @@ import "plugins/rs_infoblox_ipam"
 
 
 parameter "param_hostname" do
-    label "Host FQDN"
+    label "Host Name"
+    type "string"
+end
+
+parameter "param_domain" do
+    label "Host Domain"
     type "string"
 end
 
@@ -23,7 +28,12 @@ parameter "param_cidrblock" do
 end
 
 parameter "param_name_filter" do
-  label "Regexp to filter host name"
+  label "String to use to filter host name"
+  type "string"
+end
+
+parameter "param_search_string" do
+  label "Generic search string"
   type "string"
 end
 
@@ -33,28 +43,40 @@ end
 ##########################
 ##########################
 
-output "hostname" do
+output "host_name" do
     label "Host Name"
     category "Outputs"
-    default_value @hostrecord.host_name
+    default_value @hostrecord.name
 end
 
-output "hostaddr" do
-    label "Host Address"
+output "host_ipv4addr" do
+    label "Host IPv4 Address"
     category "Outputs"
-    default_value @hostrecord.host_addr
+    default_value @hostrecord.ipv4addr
 end
 
-output "hostref" do
+output "host_ipv6addr" do
+    label "Host IPv6 Address"
+    category "Outputs"
+    default_value @hostrecord.ipv6addr
+end
+
+output "host_ref" do
     label "Host Reference"
     category "Outputs"
-    default_value @hostrecord.host_ref
+    default_value @hostrecord._ref
 end
 
 output "output_list" do
   label "Hosts List"
   category "Hosts List"
-  default_value "Run \"More Actions\""
+  default_value "Run \"More Actions -> Test list_by_name Action\""
+end
+
+output "output_search" do
+  label "Searched Hosts List"
+  category "Hosts List"
+  default_value "Run \"More Actions -> Test search Action\""
 end
 
 ##########################
@@ -65,15 +87,24 @@ end
     
 
 resource "hostrecord", type: "rs_infoblox_ipam.record_host" do
-    name $param_hostname
+    name join([$param_hostname,".",$param_domain])
     ipv4addrs [{ ipv4addr:join(["func:nextavailableip:",$param_cidrblock]) }]
 end
 
+# Use this to test the "list_by_name" action defined in the plugin
 operation "test_list_by_name" do
   label "Test list_by_name Action"
   definition "list_by_name"
   output_mappings do {
       $output_list => $host_list
+  } end
+end
+
+operation "test_generic_search" do
+  label "Test search Action"
+  definition "generic_search"
+  output_mappings do {
+      $output_search => $searched_hosts
   } end
 end
 
@@ -85,12 +116,12 @@ end
 
 define list_by_name($param_name_filter) return $host_list do
   $hostrecords = rs_infoblox_ipam.record_host.list_by_name(name_filter: $param_name_filter)
-  call log("host records", to_s($hostrecords))
-  foreach $hostrecord in $hostrecords[0] do
-    call log("host record item", to_s($hostrecord))
-  end
-  
   $host_list = to_s($hostrecords)
+end
+
+define generic_search($param_search_string) return $searched_hosts do
+  $hostrecords = rs_infoblox_ipam.record_host.search(search_string: $param_search_string)  
+  $searched_hosts = to_s($hostrecords)
 end
 
 define log($summary, $details) do
