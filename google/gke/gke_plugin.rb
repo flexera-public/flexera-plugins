8e63aadbd867cd4d80835890a7706235f05822bc
dfrankel33 <dfrankel33@gmail.com>
2017-10-18 13:40:50 -0700

    working version

--- google/gke/gke_plugin.rb
+++ google/gke/gke_plugin.rb
@@ -29,8 +29,8 @@ plugin "gke" do
   type "clusters" do
     href_templates "{{contains(selfLink, '/clusters/') && selfLink || null}}","{{contains(selfLink, '/clusters/') && clusters[*].selfLink || null}}"
 
-    provision ""
-    delete ""
+    provision "provision_cluster"
+    delete "destroy_cluster"
 
     field "zone" do
       required true
@@ -107,5 +107,84 @@ plugin "gke" do
       type "operation"
     end 
 
+    link "targetLink" do
+      url "$targetLink"
+    end
+
     output "name","zone","operationType","status","detail","statusMessage","selfLink","targetLink","startTime","endTime"
   end 
+end
+
+resource_pool "gke" do
+  plugin $gke
+  parameter_values do
+    project $google_project
+  end
+  auth "my_google_auth", type: "oauth2" do
+    token_url "https://www.googleapis.com/oauth2/v4/token"
+    grant type: "jwt_bearer" do
+      iss cred("GOOGLE_CONTAINER_ENGINE_ACCOUNT")
+      aud "https://www.googleapis.com/oauth2/v4/token"
+      additional_claims do {
+        "scope" => "https://www.googleapis.com/auth/cloud-platform"
+      } end
+      signing_key cred("GOOGLE_CONTAINER_ENGINE_KEY")
+    end
+  end
+end
+
+define provision_cluster(@declaration) return @resource on_error: stop_debugging() do
+  call start_debugging()
+  $raw = to_object(@declaration)
+  $fields = $raw["fields"]
+  $type = $raw["type"]
+  call sys_log.set_task_target(@@deployment)
+  call sys_log.summary(join(["Provision ",$type]))
+  call sys_log.detail($raw)
+  @operation = gke.$type.create($fields)
+  call sys_log.detail(to_object(@operation))
+  sub timeout: 2m, on_timeout: skip do
+    sleep_until @operation.status == "DONE"
+  end
+  @resource = @operation.targetLink()
+  call sys_log.detail(to_object(@resource))
+  call stop_debugging()
+end
+
+define destroy_cluster(@resource) on_error: stop_debugging() do
+  call start_debugging()
+  if !empty?(@resource)
+    call sys_log.set_task_target(@@deployment)
+    call sys_log.summary(join(["Delete: ",@resource.name]))
+    @operation = @resource.destroy()
+    sub timeout: 2m, on_timeout: skip do
+      sleep_until(@operation.status == "DONE")
+    end
+    call sys_log.detail(to_object(@operation))
+  end
+end
+
+define no_operation() do
+end
+
+define skip_not_found_error() do
+  if $_error["message"] =~ "/not found/i"
+    log_info($_error["type"] + ": " + $_error["message"])
+    $_error_behavior = "skip"
+  end
+end
+
+define start_debugging() do
+  if $$debugging == false || logic_and($$debugging != false, $$debugging != true)
+    initiate_debug_report()
+    $$debugging = true
+  end
+end
+
+define stop_debugging() do
+  if $$debugging == true
+    $debug_report = complete_debug_report()
+    call sys_log.detail($debug_report)
+    $$debugging = false
+  end
+end
---
+++ google/gke/gke_test_cat.rb
@@ -0,0 +1,56 @@
+name 'GKE - Test CAT'
+rs_ca_ver 20161221
+short_description "Google Cloud Platform - GKE - Test CAT"
+import "plugins/gke"
+
+##########################
+##########################
+###### Parameters ########
+##########################
+##########################
+
+parameter "google_project" do
+    like $gke.google_project
+    default "rightscale.com:services1"
+end
+
+##########################
+##########################
+#######  Outputs  ########
+##########################
+##########################
+
+
+##########################
+##########################
+####### Resources ########
+##########################
+##########################
+
+resource "my_cluster", type: "gke.clusters" do
+  zone "us-central1-a"
+  cluster do {
+    "name" => join(["df-cluster-", last(split(@@deployment.href, "/"))]),
+    "initialNodeCount" => 3,
+    "initialClusterVersion" => "1.7.6-gke.1"
+  } end 
+end 
+
+
+
+##########################
+##########################
+###### Operations ########
+##########################
+##########################
+
+
+
+##########################
+##########################
+###### Definitions #######
+##########################
+##########################
+
+
+
