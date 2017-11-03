78953b973422f0bb3b9c7fe00abaa335f630f3e6
Richard Shade <rshade@rightscale.com>
2017-11-03 15:28:38 -0500

    updating stuff

--- libraries/acl.rb
+++ libraries/acl.rb
@@ -6,49 +6,19 @@ package "acl"
 ########################################################
 #  USAGE
 #
-#  This package provides log functions to log data to audit entries. You can use the
-#  `summary` definition to set the audit summary and the `detail` definition to add to
-#  the audit detail.
-#
-#  Before using either definition, you can optionally call `set_task_target` to specify which
-#  resource in CM you want the audits on. If you do not call this definition, the audits will be
-#  logged to the Deployment if this is used in a CloudApp in SS, or to the account if this is
-#  used outside of a CloudApp.
-#
-#  Child tasks can optionally have their own target set, for example if you have concurrent tasks
-#  that are operating on separate resource, you might want to have audits on those specific resources.
-#  Any child task that does not have its own custom target will use the target of the root task.
+#  This package provides a very rudimentary acl system for cats. 
 #
 #  EXAMPLE
 #  -----
-#  # Optionally you can set your own audit target. If you don't, it will default to the deployment (if
-#  #  this is running in a CloudApp, or to the account if running outside of SS
-#  @my_server = rs_cm.get(href: "/api/servers/1234")
-#  call sys_log.set_task_target(@my_server)
-#
-#  # Set the summary name for the audit
-#  call sys_log.summary("Audit summary")
+#  To define which groups can launch this cat, use the following code.
+#  ```
+#  import 'acl'
+#  call acl.check_user_permissions("groupname")
+#  ```
 #
 #  ------
 
 # Append to the audit entry detail for this process
-define find_my_cookies($email_id, $password_id) return $cookies do
-  call find_shard() retrieve $shard_number
-  call get_account_href() retrieve $account_href
-  $rightscale_endpoint = "https://us-" + $shard_number +".rightscale.com" 
-  $the_body={
-    "account_href": $account_href,
-    "email": $email_id,
-    "password": $password_id
-  }
-  $response = http_post(
-    url: $rightscale_endpoint + "/api/session",
-    headers: { "X_API_VERSION": "1.5" },
-    body: $the_body
-  )
-  $mycookie_information = $response["cookies"]
-end
-
 define get_account_href() return $account_href do
   $session = rs_cm.sessions.index(view: "whoami")
   $account_href = select($session[0]["links"], {"rel":"account"})[0]["href"]
@@ -70,19 +40,22 @@ end
 define get_user_groups($user_href) return $groups do
     $user_id = last(split($user_href, '/'))
     call find_shard() retrieve $shard_number
-    call find_my_cookies($email_id, $password) retrieve $cookies
     $cloud_response = http_get(
-        url: "https:/us-" + $shard_number + ".rightscale.com/grs/users/" + $user_id,
-        headers: { "X_API_VERSION": "2.0" },
-        cookies: $cookies
+        url: "https://us-" + $shard_number + ".rightscale.com/grs/users/" + $user_id + "?view=extended",
+        headers: { "X_API_VERSION": "2.0" }
     )
-    $clouds = $cloud_response["body"]
+    $body = $cloud_response["body"]
+    $group_array = $body["groups"]
+    $groups = []
+    foreach $group in $group_array do
+      $groups << $group["name"]
+    end
 end
 
-define check_user_permissions($group)
+define check_user_permissions($group) do
   call find_user_href() retrieve $user_href
   call get_user_groups($user_href) retrieve $groups
-  if logic_not(contains?($groups,$group))
+  if logic_not(contains?($groups,[$group]))
     raise "Unable to launch cat, you do not belong to the appropriate groups"
   end
 end
