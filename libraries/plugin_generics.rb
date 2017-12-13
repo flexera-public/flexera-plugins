af11dc568a9b3817eb3abacc494eb8e6e42f0130
Richard Shade <rshade@rightscale.com>
2017-12-13 10:40:26 -0600

    adding in handle_retries

--- libraries/plugin_generics.rb
+++ libraries/plugin_generics.rb
@@ -1,6 +1,7 @@
 name "Package: plugin_generics"
 rs_ca_ver 20161221
-short_description "Provides logging functionality for sending logs to audit entries in CM"
+short_description "Provides generic functions for plugins"
+long_description "Version: 1.3"
 package "plugin_generics"
 import "sys_log"
 
@@ -18,5 +19,18 @@ define stop_debugging() do
     call sys_log.summary("Debug Report")
     call sys_log.detail($debug_report)
     $$debugging = false
+  end
+end
+
+define handle_retries($task_label,$attempts, $max_attempts) do
+  if $attempts <= $max_attempts
+    sleep(10*to_n($attempts))
+    call sys_log.set_task_target(@@deployment)
+    call sys_log.summary("Task: "+$task_label+" Attempt: "+ $attempts+" of " + $max_attempts)
+    call sys_log.detail("error:"+$_error["type"] + ": " + $_error["message"])
+    log_error($_error["type"] + ": " + $_error["message"])
+    $_error_behavior = "retry"
+  else
+    raise $_errors
   end
 end
