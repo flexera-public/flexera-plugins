61ef8b062d125d4454821c2ea759ceabf2d29ba9
Richard Shade <rshade@rightscale.com>
2018-02-08 10:12:20 -0600

    PSASSET-800 - adding header for content_type=application/xml, fixes #138

--- aws/rs_aws_elasticache/elasticache_test_cat.rb
+++ aws/rs_aws_elasticache/elasticache_test_cat.rb
@@ -21,8 +21,31 @@ resource "my_subnet_group", type: "rs_aws_elasticache.subnet_group" do
   subnet_id_2 "subnet-b357c2fb"
 end
 
-resource "my_cluster", type: "rs_aws_elasticache.cluster" do
-  cache_cluster_id join(["df-",last(split(@@deployment.href, "/"))])
+resource "redis_param_group", type: "rs_aws_elasticache.parameter_group" do
+  cache_parameter_group_name join(["redis-",last(split(@@deployment.href, "/"))])
+  cache_parameter_group_family "redis2.8"
+  description join(["redis-",last(split(@@deployment.href, "/"))])
+end
+
+resource "redis_cluster", type: "rs_aws_elasticache.cluster" do
+  cache_cluster_id join(["redis-",last(split(@@deployment.href, "/"))])
+  auto_minor_version_upgrade "true"
+  az_mode "single-az"
+  cache_node_type "cache.m3.medium"
+  cache_parameter_group_name @redis_param_group.CacheParameterGroupName
+  cache_subnet_group_name @my_subnet_group.CacheSubnetGroupName
+  engine "redis"
+  engine_version "2.8.24"
+  num_cache_nodes "1"
+  preferred_availability_zone_1 "us-east-1a"
+  preferred_maintenance_window "sun:23:00-mon:01:30"
+  security_group_id_1 "sg-7dad9003"
+  tag_key_1 "foo"
+  tag_value_1 "bar"
+end
+
+resource "mem_cluster", type: "rs_aws_elasticache.cluster" do
+  cache_cluster_id join(["mem-",last(split(@@deployment.href, "/"))])
   auto_minor_version_upgrade "true"
   az_mode "cross-az"
   cache_node_type "cache.m3.medium"
@@ -42,8 +65,9 @@ operation "terminate" do
   definition "terminate"
 end
 
-define terminate(@my_param_group,@my_subnet_group,@my_sec_group,@my_cluster) do
-  delete(@my_cluster)
+define terminate(@my_param_group,@my_subnet_group,@my_sec_group,@mem_cluster,@redis_cluster) do
+  delete(@mem_cluster)
+  delete(@redis_cluster)
   sleep(600)
   concurrent do
     delete(@my_param_group)
