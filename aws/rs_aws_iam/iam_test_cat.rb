6a878ebf75a225df2845c8b21b6b8de71e2fa892
Curt Wilhelm <curt@rightscale.com>
2018-04-10 11:03:32 -0600

    add second policy

--- aws/rs_aws_iam/iam_test_cat.rb
+++ aws/rs_aws_iam/iam_test_cat.rb
@@ -28,14 +28,30 @@ resource "my_policy", type: "rs_aws_iam.policy" do
   description "test policy description"
 end
 
+resource "my_policy2", type: "rs_aws_iam.policy" do
+  name "MyTestPolicy2"
+  policy_document '{"Version":"2012-10-17","Statement":[{"Effect":"Allow","Action":"s3:ListAllMyBuckets",
+"Resource":"arn:aws:s3:::*"},{"Effect":"Allow","Action":["s3:Get*","s3:List*"],"Resource":
+["arn:aws:s3:::EXAMPLE-BUCKET2","arn:aws:s3:::EXAMPLE-BUCKET2/*"]}]}'
+  description "test policy description"
+end
+
 resource "my_role", type: "rs_aws_iam.role" do
   name 'MyTestRole'
   assume_role_policy_document '{"Version":"2012-10-17","Statement":[{"Effect":"Allow","Principal":{"Service":["ec2.amazonaws.com"]},"Action":["sts:AssumeRole"]}]}'
   description "test role description"
-  policies @my_policy.Arn
+  policies @my_policy.Arn, @my_policy2.Arn
 end
 
 resource "my_instance_profile", type:"rs_aws_iam.instance_profile" do
   name "MyInstanceProfile"
   role_name @my_role.RoleName
+end
+
+operation "terminate" do
+  definition "terminate"
+end
+
+define terminate(@my_role) do
+  delete(@my_role)
 end
