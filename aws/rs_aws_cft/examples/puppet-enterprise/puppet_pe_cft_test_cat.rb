cb371e02221aa9e90319b4861314538b28a7e427
Richard Shade <rshade@rightscale.com>
2018-05-08 08:41:36 -0500

    adding puppet cat

---
+++ aws/rs_aws_cft/examples/puppet-enterprise/puppet_pe_cft_test_cat.rb
@@ -0,0 +1,150 @@
+name "CFT Puppet Enterprise CAT"
+rs_ca_ver 20161221
+short_description  "CFT Puppet Enterprise CAT"
+long_description "Version 0.1"
+
+import "plugins/rs_aws_cft"
+
+output "out_domain_name" do
+  label "Domain Name"
+end
+
+resource "stack", type: "rs_aws_cft.stack" do
+  stack_name join(["cft-puppet-enterprise-", last(split(@@deployment.href, "/"))])
+  stack_policy_url ""
+  parameter_1_name "AssociatePublicIpAddress"
+  parameter_1_value "true"
+  parameter_2_name "CodeManagerAccess"
+  parameter_2_value "true"
+  parameter_3_name "R10KRemote"
+  parameter_3_value "git://github.com/rs-services/control-repo-example.git"
+  parameter_4_name "SecurityGroupIds"
+  parameter_4_value "sg-d94b17a8,sg-b9421ac8"
+  parameter_5_name "SSHAccess"
+  parameter_5_value "true"
+  description "CFT Puppet Enterprise"
+end
+
+operation "launch" do
+  description "Launch the application"
+  definition "launch_handler"
+end
+
+# Demonstrates how to get outputs
+# Over complicated example given the single output returned by the example CFT.
+# But hopefully it helps when faced with a CFT that returns multiple outputs.
+operation "enable" do
+  definition "post_launch"
+  output_mappings do {
+    $out_domain_name => $domain_name
+  } end
+end
+
+define launch_handler(@stack) return $cft_template,@stack do
+  call generate_cloudformation_template() retrieve $cft_template
+  task_label("provision CFT Stack")
+  $stack = to_object(@stack)
+  $stack["fields"]["template_body"] = $cft_template
+  @stack = $stack
+  provision(@stack)
+end
+
+# Outputs are provided as two arrays. 
+# One array contains the keys. The other array contains the values.
+# Given the CFT example and the fact that it returns a single input, the code below could be replaced with 
+#    $domain_name = $OutputValue[0]
+# But what's the fun in that ...?
+define post_launch(@stack) return $domain_name do
+
+  $output_keys = @stack.OutputKey
+  $output_values = @stack.OutputValue
+  
+  $i = 0
+  foreach $output_key in $output_keys do
+    if $output_key == "DomainName"
+      $domain_name = $output_values[$i]
+    elsif $output_key == "AnotherOutput"  # this will fire given the CFT example. Provided as an example bit of code.
+      $another_output = $output_values[$i]
+    end
+    $i = $i + 1
+  end
+  
+  
+end
+
+
+# Example CFT
+define generate_cloudformation_template() return $cft_template do
+  $cft_template = to_s('{ "AWSTemplateFormatVersion": "2010-09-09",
+  "Resources": {
+    "CloudFrontDistribution": {
+      "Type": "AWS::CloudFront::Distribution",
+      "Properties": {
+        "DistributionConfig": {
+          "Comment": "Final Version",
+          "Enabled": true,
+          "PriceClass": "PriceClass_All",
+          "DefaultCacheBehavior": {
+            "TargetOriginId": "myelb-1",
+            "ViewerProtocolPolicy": "allow-all",
+            "MinTTL": 0,
+            "AllowedMethods": [
+              "HEAD",
+              "DELETE",
+              "POST",
+              "GET",
+              "OPTIONS",
+              "PUT",
+              "PATCH"
+            ],
+            "CachedMethods": [
+              "HEAD",
+              "GET"
+            ],
+            "ForwardedValues": {
+              "QueryString": true,
+              "Headers": [
+                "Host",
+                "Origin"
+              ],
+              "Cookies": {
+                "Forward": "all"
+              }
+            }
+          },
+          "Origins": [
+            {
+              "DomainName": "myelb-1-123456789.us-east-1.elb.amazonaws.com",
+              "Id": "myelb-1",
+              "CustomOriginConfig": {
+                "HTTPPort": "80",
+                "HTTPSPort": "443",
+                "OriginProtocolPolicy": "http-only"
+              }
+            }
+          ],
+          "Restrictions": {
+            "GeoRestriction": {
+              "RestrictionType": "none",
+              "Locations": [
+
+              ]
+            }
+          },
+          "ViewerCertificate": {
+            "CloudFrontDefaultCertificate": "true",
+            "MinimumProtocolVersion": "TLSv1"
+          }
+        }
+      }
+    }
+  },
+  "Outputs" : {
+  "DomainName" : {
+    "Description" : "Domain Name",
+    "Value" : { "Fn::Join" : ["", ["http://", { "Fn::GetAtt" : [ "CloudFrontDistribution", "DomainName" ]}]] }
+  }
+  },
+  "Description": "Cloudfront Definition for RS"
+}')
+end
