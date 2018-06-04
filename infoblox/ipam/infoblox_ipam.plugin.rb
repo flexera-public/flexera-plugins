1e6cb693d5d31ffadfc3d26ca4cb492c1b23842d
dfrankel33 <david.frankel@rightscale.com>
2018-06-04 13:30:41 -0500

    working version

--- infoblox/ipam/infoblox_ipam.plugin.rb
+++ infoblox/ipam/infoblox_ipam.plugin.rb
@@ -10,31 +10,37 @@ name 'rs_infoblox_ipam'
 type 'plugin'
 rs_ca_ver 20161221
 short_description "Infoblox IPAM"
-long_description "Version: 0.1"
+long_description "Version: 0.2"
 package "plugins/rs_infoblox_ipam"
 import "sys_log"
 
 plugin "rs_infoblox_ipam" do
-  
+
   parameter "tunnel_token" do
     type "string"
     label "wsTunnel token"
     description "wsTunnel token"
   end
-  
+
+  parameter "api_version" do
+    type "string"
+    label "Infoblox API version"
+    description "API Version"
+  end
+
   endpoint do
     default_host "https://wstunnel1-1.rightscale.com"  # Change to wstunnel10-1 if applicable
     default_scheme "https"
-    path "_token/$tunnel_token/wapi/v2.2.2/"
+    path "_token/$tunnel_token/wapi/v$api_version/"
   end
-  
+
   # Infoblox record:host object
   # https://ipam.illinois.edu/wapidoc/objects/record.host.html
   type "record_host" do
-    href_templates "/{{_ref}}"  # The leading slash is makes the reference that comes back from infoblox href-like
-    provision "provision_record_host"
-    delete "delete_record_host"
- 
+    href_templates "/{{contains(_ref, 'record:host') && _ref || null}}" # The leading slash is makes the reference that comes back from infoblox href-like
+    provision "provision_record"
+    delete "delete_record"
+
     # The FQDN of the host to which the IP is being allocated.
     # It must contain the zone (domain) being used.
     # The infoblox service account must have write permissions to the given zone.
@@ -44,28 +50,28 @@ plugin "rs_infoblox_ipam" do
       location "body"
       required true
     end
-       
+
     # An array of hashes.
     # See the record:host_ipv4addr object definition for fields you can pass in the hashes.
     #   REF: https://ipam.illinois.edu/wapidoc/objects/record.host_ipv4addr.html
-    # 
+    #
     # To get the next available IP use this (where the CIDR is a network to which the infoblox service account has write access).
     #   ipv4addrs [{ "ipv4addr":"func:nextavailableip:10.1.124.0/24" }]
     # To get a specific IP use this (again the IP address must be in network to which the service account has access.
-    #   ipv4addrs:[{ "ipv4addr":"10.1.124.53" }]    
+    #   ipv4addrs:[{ "ipv4addr":"10.1.124.53" }]
     field "ipv4addrs" do
       type "array"
       location "body"
       required false  # An ipv4addrs object or ipv6addrs object is required
     end
-    
+
     # Like ipv4addrs - but these go up to a quinary 11
     field "ipv6addrs" do
       type "array"
       location "body"
       required false
     end
-    
+
     # The following fields are not required and represent most of the API-supported parameters.
     # Refer to https://ipam.illinois.edu/wapidoc/objects/record.host.html#fields
     field "aliases" do
@@ -73,120 +79,120 @@ plugin "rs_infoblox_ipam" do
       location "body"
       required false
     end
-    
+
     field "allow_telnet" do
       type "boolean"
       location "body"
       required false
     end
-    
+
     field "comment" do
       type "string"
       location "body"
       required false
     end
-    
+
     field "configure_for_dns" do
       type "boolean"
       location "body"
       required false
     end
-    
+
     field "device_description" do
       type "string"
       location "body"
       required false
     end
-    
+
     field "device_location" do
       type "string"
       location "body"
       required false
     end
-    
+
     field "device_type" do
       type "string"
       location "body"
       required false
     end
-    
+
     field "device_vendor" do
       type "string"
       location "body"
       required false
     end
-    
+
     field "disable" do
       type "boolean"
       location "body"
       required false
     end
-    
+
     field "disable_discovery" do
       type "boolean"
       location "body"
       required false
     end
-    
+
     field "dns_aliases" do
       type "array"
       location "body"
       required false
     end
-    
+
     field "use_ttl" do
       type "boolean"
       location "body"
       required false
     end
-    
+
     field "ttl" do
       type "number"
       location "body"
       required false
     end
-    
-    action "show", "destroy" 
-      
+
+    action "show", "destroy"
+
     action "create" do
       verb "POST"
       path "/record:host?_return_fields=ipv4addrs" # This query string is essential for json response
     end
-    
+
     # Returns an ARRAY of a single ARRAY of host record HASHES
-    # TODO: Figure out how to have it just return the array of hashes. 
+    # TODO: Figure out how to have it just return the array of hashes.
     action "list_by_name" do
       verb "GET"
       path "/record:host?name~=$name_filter"
       type "array"
-      
-      field "name_filter" do   
+
+      field "name_filter" do
         location "path"
       end
     end
-    
+
     # Generic search action. The list_by_name action above is a specific example of this.
     # Returns an ARRAY of a single ARRAY of host record HASHES
     # Examples:
     #   ipv4addr~=10\..*\.124\..* Finds any host record with an ip address of the form 10.X.124.X
-    #   name:=MyServer.example.com  Case insenstive search (:=) for any host record with a name that matches myserver.example.com 
+    #   name:=MyServer.example.com  Case insenstive search (:=) for any host record with a name that matches myserver.example.com
     # See the following document for which fields are searchable and how.
-    #   https://ipam.illinois.edu/wapidoc/objects/record.host.html#fields-list 
-    # TODO: Figure out how to have it just return the array of hashes. 
+    #   https://ipam.illinois.edu/wapidoc/objects/record.host.html#fields-list
+    # TODO: Figure out how to have it just return the array of hashes.
     action "search" do
       verb "GET"
       path "/record:host?$search_string"
       type "array"
-      
-      field "search_string" do   
+
+      field "search_string" do
         location "path"
       end
     end
-    
+
     output "_ref" do
       body_path "_ref"
     end
-    
+
     output "name" do
       body_path "name"
     end
@@ -194,12 +200,81 @@ plugin "rs_infoblox_ipam" do
     output "ipv4addr" do
       body_path "ipv4addrs[0].ipv4addr"
     end
-    
+
     output "ipv6addr" do
       body_path "ipv6addrs[0].ipv6addr"
     end
 
-  end    
+  end
+
+  type "a_record" do
+    href_templates "/{{contains(_ref, 'record:a') && _ref || null}}"
+    provision "provision_record"
+    delete "delete_record"
+
+    field "name" do
+      type "string"
+      location "body"
+      required true
+    end
+
+    field "ipv4addr" do
+      type "string"
+      location "body"
+    end
+
+    action "show", "destroy"
+
+    action "create" do
+      verb "POST"
+      path "/record:a?_return_fields=ipv4addr"
+    end
+
+    action "search" do
+      verb "GET"
+      path "/record:a?$search_string"
+      type "array"
+
+      field "search_string" do
+        location "path"
+      end
+    end
+
+    output "_ref","name","ipv4addr"
+
+  end
+
+  type "ptr_record" do
+    href_templates "/{{contains(_ref, 'record:ptr') && _ref || null}}"
+    provision "provision_record"
+    delete "delete_record"
+
+    field "name" do
+      type "string"
+      location "body"
+      required true
+    end
+
+    field "ptrdname" do
+      type "string"
+      location "body"
+    end
+
+    field "ipv4addr" do
+      type "string"
+      location "body"
+    end
+
+    action "show", "destroy"
+
+    action "create" do
+      verb "POST"
+      path "/record:ptr?_return_fields=ipv4addr"
+    end
+
+    output "_ref","name","ipv4addr","ptrdname"
+
+  end
 end
 
 resource_pool "infoblox_ipam" do
@@ -210,10 +285,11 @@ resource_pool "infoblox_ipam" do
   end
   parameter_values do
     tunnel_token "WSTUNNEL_TOKEN" # REPLACE with the wsTunnel token BEFORE uploading
+    api_version "2.6.1"
   end
 end
 
-define provision_record_host(@declaration) return @resource do
+define provision_record(@declaration) return @resource do
   sub on_error: stop_debugging() do
     call start_debugging()
     $object = to_object(@declaration)
@@ -223,8 +299,8 @@ define provision_record_host(@declaration) return @resource do
     call sys_log.set_task_target(@@deployment)
     call sys_log.summary(join(["Provision ",$type]))
     call sys_log.detail($object)
-    call sys_log.detail(join(["Host Name: ", $host_name]))
-    @operation = rs_infoblox_ipam.record_host.create($fields) 
+    call sys_log.detail(join(["Name: ", $host_name]))
+    @operation = rs_infoblox_ipam.$type.create($fields)
     call sys_log.detail("CREATE HREF: "+to_s(@operation))
     call sys_log.detail("CREATE HREF OBJECT: "+to_s(to_object(@operation)))
     @resource = @operation.get()
@@ -233,14 +309,14 @@ define provision_record_host(@declaration) return @resource do
   end
 end
 
-define delete_record_host(@declaration) do
+define delete_record(@declaration) do
   call start_debugging()
   @declaration.destroy()
   call stop_debugging()
 end
 
 define no_operation(@declaration) do
-end 
+end
 
 
 define start_debugging() do
