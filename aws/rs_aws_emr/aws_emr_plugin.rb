67676f95ed5832d07238e81cce3d0a0758411e60
Richard Shade <rshade@rightscale.com>
2018-08-23 14:27:41 -0500

    EMR JobFlow

--- aws/rs_aws_emr/aws_emr_plugin.rb
+++ aws/rs_aws_emr/aws_emr_plugin.rb
@@ -22,46 +22,131 @@ plugin "rs_aws_emr" do
     provision "provision_cluster"
     delete    "delete_resource"
 
-    field "client_request_token" do
-      alias_for "clientRequestToken"
+    field "x_amz_target" do 
+      alias_for "X-Amz-Target"
+      location "header"
       type "string"
     end
 
     field "name" do
+      alias_for "Name"
       type "string"
       required true
     end
 
-    field "resources_vpc_config" do
-      alias_for "resourcesVpcConfig"
+    field "additional_info" do
+      alias_for "AdditionalInfo"
+      type "string"
+    end
+
+    field "ami_version" do
+      alias_for "AmiVersion"
+      type "string"
+    end
+
+    field "applications" do
+      alias_for "Applications"
+      type "array"
+    end
+
+    field "auto_scaling_role" do
+      alias_for "AutoScalingRole"
+      type "string"
+    end
+
+    field "bootstrap_actions" do
+      alias_for "BootstrapActions"
+      type "array"
+    end
+
+    field "configurations" do
+      alias_for "Configurations"
+      type "array"
+    end
+
+    field "custom_ami_id" do
+      alias_for "CustomAmiId"
+      type "string"
+    end
+ 
+    field "ebs_root_volume_size" do
+      alias_for "EbsRootVolumeSize"
+      type "string"
+    end
+
+    field "instances" do
+      alias_for "Instances"
       type "composite"
-      required true
     end
 
-    field "role_arn" do
-      alias_for "roleArn"
-      type "string"
-      required true
-    end
-
-    field "version" do
+    field "job_flow_role" do
+      alias_for "JobFlowRole"
       type "string"
     end
 
-    field "next_token" do
-      alias_for "nextToken"
-      type "string"
-      location "query"
+    field "kerberos_attributes" do
+      alias_for "KerberosAttributes"
+      type "composite"
     end
 
-    action "BootstrapActions" do
-      verb "POST"
-      path "/"
+    field "log_uri" do
+      alias_for "LogUri"
+      type "string"
+    end
+
+    field "new_supported_products" do
+      alias_for "NewSupportedProducts"
+      type "array"
+    end
+
+    field "release_label" do
+      alias_for "ReleaseLabel"
+      type "string"
+    end
+
+    field "repo_upgrade_on_boot" do
+      alias_for "RepoUpgradeOnBoot"
+      type "string"
+    end
+
+    field "scale_down_behavior" do
+      alias_for "ScaleDownBehavior"
+      type "string"
+    end
+
+    field "security_configuration" do
+      alias_for "SecurityConfiguration"
+      type "string"
+    end
+
+    field "service_role" do
+      alias_for "ServiceRole"
+      type "string"
+    end
+
+    field "steps" do
+      alias_for "Steps"
+      type "array"
+    end
+
+    field "supported_products" do
+      alias_for "SupportedProducts"
+      type "array"
+    end
+
+    field "tags" do
+      alias_for "Tags"
+      type "array"
+    end
+
+    field "visible_to_all_users" do
+      alias_for "VisibleToAllUsers"
+      type "boolean"
     end
 
     action "create" do
       verb "POST"
-      path "/clusters"
+      path "/"
     end
 
     action "destroy" do
@@ -77,21 +162,55 @@ plugin "rs_aws_emr" do
     action "list" do
       verb "GET"
       path "/clusters"
-
       output_path "clusters[]"
     end
 
     output_path "cluster"
 
-    output "endpoint","status","createdAt","certificateAuthority","arn","roleArn","clientRequestToken","version","name","resourcesVpcConfig"
-
+    output "JobFlowId"
   end
+  type "job_flow" do
+    href_templates "/DescribeCluster/{{cluster.name}}"
+    provision "no_operation"
+    delete    "no_operation"
+    
+    field "created_after" do
+      alias_for "CreatedAfter"
+      type "string"
+    end
 
+    field "created_before" do
+      alias_for "CreatedBefore"
+      type "string"
+    end
+
+    field "job_flow_ids" do
+      alias_for "JobFlowIds"
+      type "array"
+    end
+
+    field "job_flow_states" do
+      alias_for "JobFlowStates"
+      type "array"
+    end
+
+    field "x_amz_target" do 
+      alias_for "X-Amz-Target"
+      location "header"
+      type "string"
+    end
+
+    action "show" do
+      verb "post"
+      path "/clusters"
+      output_path "clusters[]"
+    end
+  end
 end
 
 resource_pool "rs_aws_emr" do
   plugin $rs_aws_emr
-  host "us-east-1.elasticmapreduce.amazonaws.com"
+  host "elasticmapreduce.us-east-1.amazonaws.com"
   auth "key", type: "aws" do
     version     4
     service    'emr'
@@ -113,7 +232,8 @@ define provision_cluster(@declaration) return @resource do
     call sys_log.set_task_target(@@deployment)
     call sys_log.summary(join(["Provision ", $type]))
     call sys_log.detail($object)
-    @operation = rs_aws_emr.$type.create($fields)
+    @cluster = rs_aws_emr.$type.create($fields)
+    @operation = rs_aws_emr.job_flow.show(x_amz_target: 'ElasticMapReduce.DescribeJobFlows',job_flow_ids: [ @cluster.JobFlowId ])
     call sys_log.detail(to_object(@operation))
     sub timeout: 20m, on_timeout: skip do
       #Valid Values: STARTING | BOOTSTRAPPING | RUNNING | WAITING | TERMINATING | TERMINATED | TERMINATED_WITH_ERRORS
