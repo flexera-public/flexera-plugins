973f37a04ec35c510e36c8b7673c38001e32f223
Richard Shade <rshade@rightscale.com>
2019-03-12 14:58:34 -0500

    adding in s2s vpn cat

---
+++ azure/rs_azure_networking/s2svpn-test-cat.rb
@@ -0,0 +1,66 @@
+name 'Azure S2S VPN - Test CAT'
+rs_ca_ver 20161221
+short_description "Azure S2S VPN - Test CAT"
+import "sys_log"
+import "plugins/rs_azure_networking_plugin"
+
+parameter "subscription_id" do
+  like $rs_azure_networking_plugin.subscription_id
+end
+
+output "ip1" do
+  label "New IP"
+end
+
+permission "read_creds" do
+  actions   "rs_cm.show_sensitive","rs_cm.index_sensitive"
+  resources "rs_cm.credentials"
+end
+
+resource "resource_group", type: "rs_cm.resource_group" do
+  name @@deployment.name
+  cloud "AzureRM Central US"
+end
+
+resource "ip", type: "rs_azure_networking.public_ip_address" do
+  name join(["my-ip-", last(split(@@deployment.href, "/"))])
+  resource_group @@deployment.name
+  location "Central US"
+  properties do {
+    "publicIPAllocationMethod" => "Static",
+    "publicIPAddressVersion" => "IPv4"
+  } end
+  sku do {
+    "name" => "Standard"
+  } end
+end
+
+operation "launch" do
+ description "Launch the application"
+ definition "launch_handler"
+  output_mappings do {
+    $ip1 => $ip
+  } end
+end
+
+define launch_handler(@resource_group,@ip,$subscription_id) return @ip, $ip do
+  provision(@resource_group)
+  provision(@ip)
+  $props = @ip.properties
+  $ip = @ip.properties["ipAddress"]
+end
+
+define start_debugging() do
+  if $$debugging == false || logic_and($$debugging != false, $$debugging != true)
+    initiate_debug_report()
+    $$debugging = true
+  end
+end
+
+define stop_debugging() do
+  if $$debugging == true
+    $debug_report = complete_debug_report()
+    call sys_log.detail($debug_report)
+    $$debugging = false
+  end
+end
