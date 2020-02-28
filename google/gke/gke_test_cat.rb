88ba3b1b7e68f5c0fbcdce9cc0ca3f071e340193
Richard Shade <rshade@flexera.com>
2020-02-28 16:35:33 -0600

    adding in location items

--- google/gke/gke_test_cat.rb
+++ google/gke/gke_test_cat.rb
@@ -1,7 +1,7 @@
 name 'GKE - Test CAT'
 rs_ca_ver 20161221
 short_description "Google Cloud Platform - GKE - Test CAT"
-import "gke2"
+import "gke"
 
 ##########################
 ##########################
@@ -10,7 +10,7 @@ import "gke2"
 ##########################
 
 parameter "google_project" do
-  like $gke2.google_project
+  like $gke.google_project
   default "rightscale.com:services1"
 end
 
@@ -21,7 +21,7 @@ end
 ##########################
 
 resource "my_cluster", type: "gke.clusters" do
-  zone "us-central1-a"
+  location "us-central1-a"
   cluster do {
     "name" => join(["rs-cluster-", last(split(@@deployment.href, "/"))]),
     "initialClusterVersion" => "1.15.9-gke.9",
@@ -32,4 +32,14 @@ resource "my_cluster", type: "gke.clusters" do
       }
     ]
   } end 
-end 
+end
+
+resource "my_node_pool", type: "gke.nodePools" do
+  location "us-central1-a"
+  cluster @my_cluster.name
+  nodePool do {
+    "name" => "nodepool2",
+    "initialNodeCount" => 3
+  }
+  end
+end
