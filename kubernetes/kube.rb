fa7ad59f0e96f3a4e9b739f6da8b4ae5893e3efb
Richard Shade <rshade@flexera.com>
2020-05-11 09:10:00 -0500

    kube

--- kubernetes/kube.rb
+++ kubernetes/kube.rb
@@ -6,15 +6,6 @@ long_description "Version: 0.3"
 package "kubernetes"
 import "sys_log"
 
-permission "pft_general_permissions" do
-  resources "rs_cm.tags", "rs_cm.instances", "rs_cm.audit_entries", "rs_cm.credentials", "rs_cm.clouds", "rs_cm.sessions", "rs_cm.accounts", "rs_cm.publications"
-  actions   "rs_cm.*"
-end
-
-permission "pft_sensitive_views" do
-  resources "rs_cm.credentials" # Currently these actions are not support for instance resources, "rs_cm.instances"
-  actions "rs_cm.index_sensitive", "rs_cm.show_sensitive"
-end
 
 parameter "kubernetes_host" do
   type "string"
@@ -223,48 +214,4 @@ define stop_debugging() do
     call sys_log.detail($debug_report)
     $$debugging = false
   end
-end
-
-output "output_pods" do
-  label "Pods"
-end
-
-output "output_pod_names" do
-  label "Pod Names"
-end
-
-output "output_containers" do
-  label "Containers"
-end
-
-operation "get_pods" do
-  definition "get_pods"
-  output_mappings do {
-    $output_pods => $pods,
-    $output_pod_names => $podnames,
-    $output_containers => $containers
-  } end
-end
-
-define get_pods() return @pods,$pods,$podnames,$containers do
-  call sys_log.summary("List Pods")
-  call sys_log.detail("running get pods")
-  call start_debugging()
-  @pods = kubernetes.pods.empty()
-  sub on_error: stop_debugging() do
-    @pods = kubernetes.pods.list()
-  end
-  call sys_log.detail(to_s(@pods))
-  call stop_debugging()
-  $pods = to_s(@pods)
-  $arr_pod_names = []
-  $arr_container_names = []
-  foreach @pod in @pods do
-    $arr_pod_names << @pod.name
-    foreach $container in @pod.containers do
-      $arr_container_names << $container["name"]
-    end
-  end
-  $podnames = to_s($arr_pod_names)
-  $containers = to_s($arr_container_names)
 end
