3c8c60aa118cf5c947ff82ffef1a5fb96c9b54bd
Richard Shade <rshade@flexera.com>
2021-08-03 09:23:08 -0500

    updating with changes from helping Maurice

--- azure/rs_azure_compute/azure_compute_plugin.rb
+++ azure/rs_azure_compute/azure_compute_plugin.rb
@@ -1,4 +1,4 @@
-name 'Azure Compute'
+name 'Plugin: Azure Compute'
 type 'plugin'
 rs_ca_ver 20161221
 short_description "Azure Compute"
@@ -45,7 +45,7 @@ plugin "azure_compute" do
     label 'Source'
     url 'https://github.com/flexera/flexera-plugins/blob/master/azure/rs_azure_compute/azure_compute_plugin.rb'
   end
-  
+
   documentation_link 'readme' do
     label 'Readme'
     url 'https://github.com/flexera/flexera-plugins/blob/master/azure/rs_azure_compute/README.md'
@@ -55,15 +55,15 @@ plugin "azure_compute" do
     default_host "https://management.azure.com/"
     default_scheme "https"
     query do {
-      'api-version' =>  '2019-07-01'
+      'api-version' =>  '2021-03-01'
     } end
   end
-  
+
   parameter "tenant_id" do
     type "string"
     label "Tenant ID"
   end
-  
+
   parameter "subscription_id" do
     type  "string"
     label "subscription_id"
@@ -178,6 +178,11 @@ plugin "azure_compute" do
       location "body"
     end
 
+    field "tags" do
+      type "composite"
+      location "body"
+    end
+
     action "create" do
       type "virtualmachine"
       path "/subscriptions/$subscription_id/resourceGroups/$resource_group/providers/Microsoft.Compute/virtualMachines/$name"
@@ -221,8 +226,8 @@ plugin "azure_compute" do
       type "virtualmachine"
       path "/subscriptions/$subscription_id/providers/Microsoft.Compute/virtualMachines"
       verb "GET"
-      pagination $azure_pagination  
-      output_path "value[*]"  
+      pagination $azure_pagination
+      output_path "value[*]"
     end
 
     action "stop" do
@@ -255,8 +260,8 @@ plugin "azure_compute" do
     end
 
     polling do
-      field_values do   
-      end    
+      field_values do
+      end
       period 60
       action 'list_all'
     end
@@ -315,6 +320,11 @@ plugin "azure_compute" do
     end
 
     field "protectedSettings" do
+      type "composite"
+      location "body"
+    end
+
+    field "tags" do
       type "composite"
       location "body"
     end
@@ -394,6 +404,11 @@ plugin "azure_compute" do
       location "body"
     end
 
+    field "tags" do
+      type "composite"
+      location "body"
+    end
+
     action "create" do
       type "scale_set"
       verb "PUT"
@@ -435,7 +450,7 @@ plugin "azure_compute" do
       body_path "properties.provisioningState"
     end
   end
-  
+
    type "snapshots" do
     href_templates "{{value[*].properties.sourceUniqueId}}"
     provision "no_operation"
@@ -446,7 +461,7 @@ plugin "azure_compute" do
       path "/subscriptions/$subscription_id/providers/Microsoft.Compute/snapshots"
       verb "GET"
       output_path "value[*]"
-      pagination $azure_pagination    
+      pagination $azure_pagination
     end
 
     output 'id' do
@@ -483,7 +498,7 @@ plugin "azure_compute" do
 
     polling do
       field_values do
-      end  
+      end
       period 60
       action 'list'
     end
@@ -550,12 +565,18 @@ plugin "azure_compute" do
       verb "GET"
     end
 
+    action "update" do
+      type "disks"
+      path "$href"
+      verb "PATCH"
+    end
+
     action "list" do
       type "disks"
       path "/subscriptions/$subscription_id/providers/Microsoft.Compute/disks"
       verb "GET"
       output_path "value[*]"
-      pagination $azure_pagination    
+      pagination $azure_pagination
     end
 
     action "destroy" do
@@ -588,12 +609,12 @@ plugin "azure_compute" do
 
     polling do
       field_values do
-      end  
+      end
       period 60
       action 'list'
     end
   end
-  
+
    type "images" do
     href_templates "{{value[*].name}}"
     provision "no_operation"
@@ -606,7 +627,7 @@ plugin "azure_compute" do
       output_path "value[*]"
       pagination $azure_pagination
     end
-  
+
     output 'id' do
      body_path 'id'
     end
@@ -625,15 +646,15 @@ plugin "azure_compute" do
 
     output 'tags' do
      body_path 'tags'
-    end  
+    end
 
     polling do
       field_values do
-    end  
+    end
       period 60
     action 'list'
     end
-  end 
+  end
 end
 
 resource_pool "azure_compute" do
@@ -807,3 +828,4 @@ define stop_debugging() do
     $$debugging = false
   end
 end
+
