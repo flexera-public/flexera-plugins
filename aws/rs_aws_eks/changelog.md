d920babe186efa10d9c7350834957620457c6442
Richard Shade <131337+rshade@users.noreply.github.com>
2022-01-14 10:11:25 -0600

    POL-197 - AWS EKS (#181)
    * Initial cut for CE
    
    * adding in eks items
    
    * Adding in polling
    
    * updating version
    
    * updating items
    
    * EKS Plugin
    
    * list-node-groups
    
    * more updates
    
    * rename file to .plugin
    
    * Updating EKS
    
    * updating system
    
    * commiting all changes
    
    commiting all changes
    
    * changelog
    
    * plugin update
    
    * updating
    
    * info block
    
    * updating per peer review
    
    * tabs not spaces
    
    * other cleanup
    
    * more tab fix
    
    * fixing 180
    
    Co-authored-by: Curt Wilhelm <1047804+cdwilhelm@users.noreply.github.com>
    Co-authored-by: gopinathmahakud1 <55230965+gopinathmahakud1@users.noreply.github.com>

--- aws/rs_aws_eks/changelog.md
+++ aws/rs_aws_eks/changelog.md
@@ -1,5 +1,9 @@
 # EKS Plugin changelog
 
+## v2.0.0
+
+- CRB Changes
+
 ## v1.0
 
 - initial release
--- aws/rs_aws_eks/eks_test_cat.rb
+++ aws/rs_aws_eks/eks_test_cat.rb
@@ -1,18 +1,32 @@
 name 'EKS Test CAT'
 rs_ca_ver 20161221
 short_description "Amazon Web Services - EKS"
-import "plugins/rs_aws_eks"
+import "aws_eks"
 
+parameter "region" do
+  like $aws_eks.region
+end
 
-resource "my_cluster", type: "rs_aws_eks.clusters" do
-  name "my_kube_cluster"
+resource "my_cluster", type: "aws_eks.clusters" do
+  name join(["my_kube_cluster", last(split(@@deployment.href, "/"))])
   resources_vpc_config do {
     "securityGroupIds" => ["sg-7dad9003"],
     "subnetIds" => ["subnet-b357c2fb","subnet-bb06b7e1"],
     "vpcId" => "vpc-8172a6f8"
   } end
   role_arn "arn:aws:iam::041819229125:role/DF-EKS-Role"
-  version "1.10"
+  version "1.14"
+end
+
+resource "my_nodegroup", type: "aws_eks.nodegroups" do
+  name join(["my_node_group", last(split(@@deployment.href, "/"))])
+  cluster_name @my_cluster.name
+  amiType "AL2_x86_64"
+  nodeRole "arn:aws:iam::041819229125:role/rs-sre-staging-us-east-2-worker-node-iam"
+  subnets do [
+    "subnet-b357c2fb",
+    "subnet-bb06b7e1"
+  ] end
 end
 
 output "out_endpoint" do
